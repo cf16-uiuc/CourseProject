988dc65ca398205e7f59ee69bb5343d99adb87ee
cf16-uiuc <70982495+cf16-uiuc@users.noreply.github.com>
2020-12-13 23:49:31 -0500

    Updating test.py

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+# import necessary packages
+
 from simpletransformers.classification import ClassificationModel
 import pandas as pd
 import json
@@ -7,6 +9,9 @@ from gensim.parsing.preprocessing import remove_stopwords
 # Initialize variables
 
 DATA_PATH = './data/test.jsonl'
+FINAL_RESULTS = 'answer.txt'
+MODEL_LOCATION = 'outputs'
+MODEL_TYPE = 'bert'
 
 word_dist = []
 pred = []
@@ -18,7 +23,7 @@ def convert_emojis(text):
     return text
 
 
-def predict_sarcasm(data_path):
+def predict_sarcasm(data_path, results, model_loc, model):
     # Bringing in the test data
     with open(data_path, 'r') as json_file:
         json_list = list(json_file)
@@ -29,7 +34,7 @@ def predict_sarcasm(data_path):
     pred_response = [remove_stopwords(convert_emojis(pred[i]['response'])) for i in range(len(pred))]
     pred_id = [pred[i]['id'] for i in range(len(pred))]
 
-    model = ClassificationModel("electra", "outputs", use_cuda=False)
+    model = ClassificationModel(model, model_loc, use_cuda=False)
 
     predictions, raw_outputs = model.predict(pred_response)
 
@@ -39,6 +44,7 @@ def predict_sarcasm(data_path):
     })
 
     pred_bert['label'] = pred_bert['label'].replace([1, 0], ['SARCASM', 'NOT_SARCASM'])
+    pd.DataFrame(pred_bert).to_csv(results, header=False, sep=',', index=False)
 
 
-predict_sarcasm(DATA_PATH)
+predict_sarcasm(DATA_PATH, FINAL_RESULTS, MODEL_LOCATION, MODEL_TYPE)
